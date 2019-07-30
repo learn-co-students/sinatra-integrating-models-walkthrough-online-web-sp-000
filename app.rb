782b8f68c02228095fed15e657a284f6bc611d8c
Irina Kagan <irinagamb@gmail.com>
2019-07-30 00:20:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,13 +2,18 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
+
   get '/' do
+
+    @user_text = "Number of Words: 4"
     erb :index
   end
 
   post '/' do
     text_from_user = params[:user_text]
 
-    erb :results
+      @analyzed_text = TextAnalyzer.new(text_from_user)
+
+  erb :results
   end
 end

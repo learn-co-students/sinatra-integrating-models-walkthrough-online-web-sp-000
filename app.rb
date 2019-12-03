31e2ba16245282b10ba8bfdf85c436d8dbe8fbee
Connor Cowan <ccowan003@comcast.net>
2019-12-03 15:01:30 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+require_relative "models/text_analyzer.rb"
 
 class App < Sinatra::Base
   get '/' do
     erb :index
   end
 
+  #TextAnalyzer saved to an instance variable called @analyzed_text
   post '/' do
-    text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
-  end
+  #end
 end

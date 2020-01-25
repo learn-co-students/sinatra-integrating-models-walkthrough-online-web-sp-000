eb92c4f3594129285b6434c77971f6148f60c259
Vesna Vucinic <vesna.vucinic68@gmail.com>
2020-01-25 19:22:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,20 +1,14 @@
 require_relative 'config/environment'
-require_relative 'models/text_analyzer.rb' #This gives us the ability to reference the TextAnalyzer class and invoke its new method.
+require_relative 'models/text_analyzer.rb' 
 
 class App < Sinatra::Base
-  get '/' do #rout which renders the index.erb page
+  get '/' do
     erb :index
   end
 
-  post '/' do #rout which recives data through params and renders the resullts page
-    #text_from_user = params[:user_text]
-    #@analyzed_text = TextAnalyzer.new(text_from_user)
-    # 10 and 11 rows are shortened to:
-    @analyzed_text = TextAnalyzer.new(params[:user_text]) #We have the instance of TextAnalyzer saved to an instance variable called @analyzed_text
-                                                          #This means that we can call it and its methods from the results.erb view that is being rendered, using erb tags!
-    erb :results  #in our results.erb file, use erb tags to display the data stored in the @analyzed_text variable.
+  post '/' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+
+    erb :results
   end
 end
-# Sinatra app that uses a model, views, and a controller!
-# You are taking user input in a form, sending it via params to the 'post' route where a new instance of the model is created using the data from the form.
-# This instance is passed back to the view, where it is rendered using erb tags. 

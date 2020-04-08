594bc2457df1fc1388181dec16b80bb3166d2022
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-08 07:08:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @count_of_words = @analyzed_text.count_of_words
+    @count_of_vowels = @analyzed_text.count_of_vowels
+    @count_of_consonants = @analyzed_text.count_of_consonants
+    @most_used_letter = @analyzed_text.most_used_letter.first[0].upcase
+    @most_used_letter_count = @analyzed_text.most_used_letter.first[1].to_i
+# binding.pry
     erb :results
   end
 end

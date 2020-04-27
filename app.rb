d7bbcd250a2b9f40eb1ea9322bd0f7b9a43e305e
Brian Vehige <brianvehige@gmail.com>
2020-04-27 14:10:55 -0700

    Done.

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+    @array = @analyzed_text.most_used_letter.shift
     erb :results
   end
 end

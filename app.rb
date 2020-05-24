e75d0455fdd6b75f31c523ad27de1fae8391aa91
dayofgray <graysonpberger@gmail.com>
2020-05-23 21:00:40 -0400

    Revert "Done."
    This reverts commit 483489cd78adf63bed23a10eae95ed1d2e517145.

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @analyzed_text = TextAnalyzer.new(params[:user_text])
-  
+    text_from_user = params[:user_text]
+
     erb :results
   end
 end
